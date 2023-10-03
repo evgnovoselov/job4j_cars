8c3f3c04ab1026ec9b7d297fcc290713dd9c8301
Evgeny Novoselov <enovoselov@yahoo.com>
2023-10-03 21:28:12 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -0,0 +1,28 @@
+package ru.job4j.cars.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "auto_post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private String description;
+    private LocalDateTime created;
+    @ManyToOne
+    @JoinColumn(name = "auto_user_id")
+    private User user;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "auto_post_id")
+    private List<PriceHistory> priceHistories = new ArrayList<>();
+}
---
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -0,0 +1,18 @@
+package ru.job4j.cars.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "price_history")
+public class PriceHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private long before;
+    private long after;
+}
