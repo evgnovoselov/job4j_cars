107626568f5dab463b445a19b8431ee89dbe955f
Evgeny Novoselov <enovoselov@yahoo.com>
2024-04-08 22:36:37 +0300

    Добавил методы в репозиторий постов

--- src/main/java/ru/job4j/cars/repository/PostRepository.java
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -1,4 +1,13 @@
 package ru.job4j.cars.repository;
 
+import ru.job4j.cars.model.Post;
+
+import java.util.Collection;
+
 public interface PostRepository {
+    Collection<Post> findAllByCreatedLastDay();
+
+    Collection<Post> findAllWherePhotoIs(boolean have);
+
+    Collection<Post> findAllByCarNameLike(String name);
 }
--- src/main/java/ru/job4j/cars/repository/hibernate/HibernatePostRepository.java
+++ src/main/java/ru/job4j/cars/repository/hibernate/HibernatePostRepository.java
@@ -2,10 +2,29 @@ package ru.job4j.cars.repository.hibernate;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
+import ru.job4j.cars.model.Post;
 import ru.job4j.cars.repository.PostRepository;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Repository
 @AllArgsConstructor
 public class HibernatePostRepository implements PostRepository {
     private final CrudRepository crudRepository;
+
+    @Override
+    public Collection<Post> findAllByCreatedLastDay() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Collection<Post> findAllWherePhotoIs(boolean have) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Collection<Post> findAllByCarNameLike(String name) {
+        return Collections.emptyList();
+    }
 }
