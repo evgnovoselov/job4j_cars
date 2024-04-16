28cf4ee6351092b211c4679d61f56a703cc79f6b
Evgeny Novoselov <enovoselov@yahoo.com>
2024-04-16 14:15:26 +0300

    Поправил HibernatePostRepository

--- src/main/java/ru/job4j/cars/repository/hibernate/HibernatePostRepository.java
+++ src/main/java/ru/job4j/cars/repository/hibernate/HibernatePostRepository.java
@@ -2,6 +2,7 @@ package ru.job4j.cars.repository.hibernate;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import ru.job4j.cars.model.Post;
 import ru.job4j.cars.repository.PostRepository;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @AllArgsConstructor
@@ -17,41 +19,44 @@ import java.util.List;
 public class HibernatePostRepository implements PostRepository {
     private final CrudRepository crudRepository;
 
+    private Collection<Post> findAllPost(String whereJpql, Map<String, Object> parameters) {
+        return crudRepository.tx(session -> {
+            String jpql;
+            List<Post> posts;
+            jpql = """
+                    select p from Post p
+                    left join fetch p.photo
+                    left join fetch p.user
+                    left join fetch p.car c
+                    left join fetch c.engine
+                    left join fetch c.historyOwners
+                    where %s""".formatted(whereJpql);
+            Query<Post> query = session.createQuery(jpql, Post.class);
+            for (Map.Entry<String, Object> parameterEntry : parameters.entrySet()) {
+                query.setParameter(parameterEntry.getKey(), parameterEntry.getValue());
+            }
+            posts = query.getResultList();
+            jpql = """
+                    select p from Post p
+                    left join fetch p.priceHistories
+                    where p in :posts""";
+            posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
+                    .getResultList();
+            jpql = """
+                    select p from Post p
+                    left join fetch p.participates
+                    where p in :posts
+                    order by p.created desc""";
+            posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
+                    .getResultList();
+            return posts;
+        });
+    }
+
     @Override
     public Collection<Post> findAllByCreatedBetween(LocalDateTime from, LocalDateTime to) {
         try {
-            return crudRepository.tx(session -> {
-                String jpql = """
-                        select p from Post p
-                        left join fetch p.photo
-                        left join fetch p.user
-                        left join fetch p.car c
-                        left join fetch c.engine
-                        where p.created between :from and :to""";
-                List<Post> posts = session.createQuery(jpql, Post.class)
-                        .setParameter("from", from).setParameter("to", to).getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.car c
-                        left join fetch c.historyOwners
-                        where p in :posts""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.priceHistories
-                        where p in :posts""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.participates
-                        where p in :posts
-                        order by p.created desc""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                return posts;
-            });
+            return findAllPost("p.created between :from and :to", Map.of("from", from, "to", to));
         } catch (Exception e) {
             log.error("Error find all by created between from {} to {}", from, to);
             log.error(e.getMessage());
@@ -62,39 +67,10 @@ public class HibernatePostRepository implements PostRepository {
     @Override
     public Collection<Post> findAllWherePhotoIsNotNull() {
         try {
-            return crudRepository.tx(session -> {
-                String jpql = """
-                        select p from Post p
-                        left join fetch p.photo
-                        left join fetch p.user
-                        left join fetch p.car c
-                        left join fetch c.engine
-                        where p.photo is not null""";
-                List<Post> posts = session.createQuery(jpql, Post.class).getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.car c
-                        left join fetch c.historyOwners
-                        where p in :posts""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.priceHistories
-                        where p in :posts""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.participates
-                        where p in :posts
-                        order by p.created desc""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                return posts;
-            });
+            return findAllPost("p.photo is not null", Map.of());
         } catch (Exception e) {
             log.error("Error find all by created last day");
+            log.error(e.getMessage());
         }
         return Collections.emptyList();
     }
@@ -102,41 +78,10 @@ public class HibernatePostRepository implements PostRepository {
     @Override
     public Collection<Post> findAllByCarNameLike(String name) {
         try {
-
-            return crudRepository.tx(session -> {
-                String jpql = """
-                        select p from Post p
-                        left join fetch p.photo
-                        left join fetch p.user
-                        left join fetch p.car c
-                        left join fetch c.engine
-                        where lower(c.name) like lower(:name)""";
-                List<Post> posts = session.createQuery(jpql, Post.class)
-                        .setParameter("name", "%" + name + "%").getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.car c
-                        left join fetch c.historyOwners
-                        where p in :posts""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.priceHistories
-                        where p in :posts""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                jpql = """
-                        select p from Post p
-                        left join fetch p.participates
-                        where p in :posts
-                        order by p.created desc""";
-                posts = session.createQuery(jpql, Post.class).setParameter("posts", posts)
-                        .getResultList();
-                return posts;
-            });
+            return findAllPost("lower(c.name) like lower(:name)", Map.of("name", "%" + name + "%"));
         } catch (Exception e) {
             log.error("Error find all by car name");
+            log.error(e.getMessage());
         }
         return Collections.emptyList();
     }
